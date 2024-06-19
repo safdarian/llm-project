4aad6fddeb56bb52e7d1d7e80a37251b10501874
safdarian <safdarian2000@gmail.com>
2024-06-20 00:59:30 +0330

    add config manager

--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
 from langgraph.graph import StateGraph, END
 from typing_extensions import Annotated, TypedDict
 from langchain_core.runnables import RunnableConfig
-
+import json
 
 class State(TypedDict):
      question: str
-     db_schema: dict
-     text_2_sql_results: dict
+     db_data: dict
+     text2sql_results: dict
      data_analytics_results: dict
      plot_generator_results: dict
      data_storytelling_results: dict
      report_generation_results: dict
      
+class ConfigManager(dict):
+    def __init__(self) -> None:
+        super().__init__()
+        with open("configs.json") as f:
+            self.update(json.loads(f.read()))
+    
+    
