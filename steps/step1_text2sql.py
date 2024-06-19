3fec645677b67071a0b71bace9df7388cf552c95
safdarian <safdarian2000@gmail.com>
2024-06-19 20:52:40 +0330

    add Nodes initial template

---
+++ steps/step1_text2sql.py
@@ -0,0 +1,19 @@
+from typing import Any
+from utils import State
+
+
+class Node:
+    def __init__(self) -> None:
+        pass
+
+    def forward(self, input_data: State):
+        return {"test": "ok"}
+    
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.forward(*args, **kwds)
+    
+
+
+if __name__ == "__main__":
+    c = Node()
+    print(c())
