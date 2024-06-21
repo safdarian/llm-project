25aa8178cb6864df49851a2d8d17dc5a41e672ec
safdarian <safdarian2000@gmail.com>
2024-06-21 04:08:08 +0330

    add db executions to text2sql

--- steps/step1_text2sql.py
+++ steps/step1_text2sql.py
@@ -3,12 +3,13 @@ from utils import State, ConfigManager, LLM
 from langchain_together import Together
 import re
 import os
-
+from db_manager import DBManager
+import pandas as pd
 
 class Node:
-    def __init__(self, db_info) -> None:
+    def __init__(self) -> None:
         self.config = ConfigManager()
-        self.db_info = db_info
+        self.db = DBManager(self.config["database"])
         self.llm = LLM("togetherAI")
     def forward(self, state: State):
         question = state.get("question")
@@ -19,12 +20,16 @@ class Node:
         put answer in this format:
         ```sql query```"""
         
-        answer = self.llm(template.format(self.db_info["schema"], question))
+        answer = self.llm(template.format(self.db.get_schema(), question))
         if len(re.findall(r"```sql(.*)```", answer, re.DOTALL)) > 0:
-            answer = re.findall(r"```sql(.*?)```", answer, re.DOTALL)[0].strip()
-        # ans_dict = copy.copy(state)
-        csv_path = os.path.join("outputs", "business_sales_data_updated.csv")
-        state["text2sql_results"] = {"query": answer,
+            query = re.findall(r"```sql(.*?)```", answer, re.DOTALL)[0].strip()
+        
+        results = self.db.query(query)
+        df = pd.DataFrame(results)
+        df.to_csv('data.csv', index=False)
+        # csv_path = os.path.join("outputs", "data.csv")
+        csv_path = "data.csv"
+        state["text2sql_results"] = {"query": query,
                                           "csv_path": csv_path
                                           }
         return state
