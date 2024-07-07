e854e0944cb1a611fab93fb4072f9a25a28b64b1
m-salmani78 <m.salmani78@ut.ac.ir>
2024-07-07 20:04:15 +0330

    move plot task to PlotGeneratorNode

--- steps/step2_data_analytics.py
+++ steps/step2_data_analytics.py
@@ -17,36 +17,36 @@ class DataAnalyticsNode:
         # self.llm = LLM("cluade")
 
     def forward(self, state: AgentState):
-        LoggerManager.log_flow("Start Data Analytics Node", node=self.__class__.__name__, state=LogState.START)
-        
+        LoggerManager.log_flow(f"Start with {self.llm.model_name}", node=self.__class__.__name__, state=LogState.START)
+
         # Load the CSV file
         csv_path = state["text2sql_results"]["csv_path"]
         df = pd.read_csv(csv_path)
-        
+
         # Generate summary of the CSV file
         summary_text = self.generate_summary_text(df)
-        
+
         # Formulate hypothesis based on the summary and user question
-        user_question = state.get("question")
+        user_question = state["question"]
         hypothesis = self.formulate_hypothesis(summary_text, user_question)
-        
+
         # Update state with summary and hypothesis
         state["data_analytics_results"] = {"summary_text": summary_text, "hypothesis": hypothesis}
-        
+
         LoggerManager.log_flow(f"State updated with data analytics results:\n{state['data_analytics_results']}", node=self.__class__.__name__, state=LogState.RESPONSE)
         return state
-    
+
     def generate_summary_text(self, df: pd.DataFrame) -> str:
         summary_lines = []
-        
+
         summary_lines.append(f"Shape of the data: {df.shape}")
-        
+
         summary_lines.append("Columns with sample data and their data types:")
         for col in df.columns:
             dtype = df[col].dtype
             sample = df[col].iloc[0]
             summary_lines.append(f"  - {col}: {sample} ({dtype})")
-            
+
         return "\n".join(summary_lines)
 
     def formulate_hypothesis(self, summary_text: str, user_question: str) -> str:
@@ -59,10 +59,9 @@ class DataAnalyticsNode:
 
         result = chain.invoke({"user_question": user_question, "summary_text": summary_text})
         return result
-    
+
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         return self.forward(*args, **kwds)
-    
 
 
 if __name__ == "__main__":
