838c4a2eee9ea05cbaa0c45c3b535c357d8b8fee
m-salmani78 <m.salmani78@ut.ac.ir>
2024-07-08 03:53:11 +0330

    add exception handling

--- steps/step3_plot_generator.py
+++ steps/step3_plot_generator.py
@@ -19,64 +19,67 @@ class PlotGeneratorNode:
 
     def forward(self, state: AgentState):
         LoggerManager.log_flow(f"Start with {self.llm.model_name}", node=self.__class__.__name__, state=LogState.START)
-        csv_file = state["text2sql_results"]["csv_path"]
-        user_query = state["question"]
-        hypothesis = state["data_analytics_results"]["hypothesis"]
-        csv_summery = state["data_analytics_results"]["summary_text"]
-        plot_generator_results = {}
-        df = pd.read_csv(csv_file)
-        num_rows = len(df)
-        columns = df.columns.tolist()
-        columns_text = "The CSV file Columns are: " + ", ".join(columns)
-        LoggerManager.log_flow(f"CSV file loaded with {num_rows} rows and columns: {columns}", node=self.__class__.__name__)
+        try:
+            csv_file = state["text2sql_results"]["csv_path"]
+            user_query = state["question"]
+            hypothesis = state["data_analytics_results"]["hypothesis"]
+            csv_summery = state["data_analytics_results"]["summary_text"]
+            plot_generator_results = {}
+            df = pd.read_csv(csv_file)
+            num_rows = len(df)
+            columns = df.columns.tolist()
+            columns_text = "The CSV file Columns are: " + ", ".join(columns)
+            LoggerManager.log_flow(f"CSV file loaded with {num_rows} rows and columns: {columns}", node=self.__class__.__name__)
 
-        #Tha main scenario is when the CSV file has more than one row
-        if num_rows > 1:
-            parser = JsonOutputParser(pydantic_object=TextAndCode)
+            #Tha main scenario is when the CSV file has more than one row
+            if num_rows > 1:
+                parser = JsonOutputParser(pydantic_object=TextAndCode)
 
-            prompt = PromptTemplate(
-                template="Answer the user query.\n{format_instructions}\n{user_prompt}\n{hypothesis}\n{csv_summery}",
-                input_variables=["user_prompt", "hypothesis", "csv_summery"],
-                partial_variables={"format_instructions": parser.get_format_instructions()},
-            )
+                prompt = PromptTemplate(
+                    template="Answer the user query.\n{format_instructions}\n{user_prompt}\n{hypothesis}\n{csv_summery}",
+                    input_variables=["user_prompt", "hypothesis", "csv_summery"],
+                    partial_variables={"format_instructions": parser.get_format_instructions()},
+                )
 
-            chain = prompt | self.llm | parser
+                chain = prompt | self.llm | parser
 
-            result = chain.invoke({"user_prompt": user_query, "hypothesis":hypothesis,"csv_summery": csv_summery})
-            plot_generator_results["answer"] = result["intro"]
-            plot_generator_results["plot_code"] = result["code"]
-            LoggerManager.log_flow(f"Plot code generated:\n```python\n{result['code']}\n```", node=self.__class__.__name__, state=LogState.RESPONSE)
-        
-        #The edge case is when the CSV file has only one row
-        else:
-            output_parser = StrOutputParser()
-            row = df.to_dict(orient='records')[0]
-            firstRow_text = "Row data: " + ", ".join([f"{col}: {row[col]}" for col in columns]) + "\n"
-            prompt = PromptTemplate(
-                template="Answer the user query based on the retrived information.\n{format_instructions}\n{user_prompt}\n{columns_text}\n{firstRow_text}",
-                input_variables=["user_prompt", "columns_text", "firstRow_text"],
-                partial_variables={"format_instructions": output_parser.get_format_instructions()},
-            )
-            chain = prompt | self.llm | output_parser
-            result = chain.invoke({"user_prompt": user_query, "columns_text": columns_text, "firstRow_text": firstRow_text})
-            plot_generator_results["answer"] = result[0]
-            LoggerManager.log_flow(f"Answer generated for single row data:\n{result[0]}", node=self.__class__.__name__, state=LogState.RESPONSE)
+                result = chain.invoke({"user_prompt": user_query, "hypothesis":hypothesis,"csv_summery": csv_summery})
+                plot_generator_results["answer"] = result["intro"]
+                plot_generator_results["plot_code"] = result["code"]
+                LoggerManager.log_flow(f"Plot code generated:\n```python\n{result['code']}\n```", node=self.__class__.__name__, state=LogState.RESPONSE)
             
-        LoggerManager.log_flow(f"State updated with plot generator results:\n{plot_generator_results}", node=self.__class__.__name__)
+            #The edge case is when the CSV file has only one row
+            else:
+                output_parser = StrOutputParser()
+                row = df.to_dict(orient='records')[0]
+                firstRow_text = "Row data: " + ", ".join([f"{col}: {row[col]}" for col in columns]) + "\n"
+                prompt = PromptTemplate(
+                    template="Answer the user query based on the retrived information.\n{format_instructions}\n{user_prompt}\n{columns_text}\n{firstRow_text}",
+                    input_variables=["user_prompt", "columns_text", "firstRow_text"],
+                    partial_variables={"format_instructions": output_parser.get_format_instructions()},
+                )
+                chain = prompt | self.llm | output_parser
+                result = chain.invoke({"user_prompt": user_query, "columns_text": columns_text, "firstRow_text": firstRow_text})
+                plot_generator_results["answer"] = result[0]
+                LoggerManager.log_flow(f"Answer generated for single row data:\n{result[0]}", node=self.__class__.__name__, state=LogState.RESPONSE)
+                
+            LoggerManager.log_flow(f"State updated with plot generator results:\n{plot_generator_results}", node=self.__class__.__name__)
 
-        LoggerManager.log_flow(f"Started", node='PlotCode', state=LogState.START)
-        code = plot_generator_results["plot_code"]
-        code = re.sub(r"plt\.show\(\)", "", code)
-        plt.switch_backend('Agg')
-        exec(code)
-        plot_id = len(glob("static/plot_*.png"))
-        plot_filename = os.path.join("static", f"plot_{plot_id}.png")
-        plt.savefig(plot_filename)
-        plt.close()
-        plot_generator_results["plot_filename"] = plot_filename
-        state["plot_generator_results"] = plot_generator_results
+            LoggerManager.log_flow(f"Started", node='PlotCode', state=LogState.START)
+            code = plot_generator_results["plot_code"]
+            code = re.sub(r"plt\.show\(\)", "", code)
+            plt.switch_backend('Agg')
+            exec(code)
+            plot_id = len(glob("static/plot_*.png"))
+            plot_filename = os.path.join("static", f"plot_{plot_id}.png")
+            plt.savefig(plot_filename)
+            plt.close()
+            plot_generator_results["plot_filename"] = plot_filename
+            state["plot_generator_results"] = plot_generator_results
 
-        LoggerManager.log_flow(f"Save plot in: {plot_filename}", node='PlotCode', state=LogState.FINISH)
+            LoggerManager.log_flow(f"Save plot in: {plot_filename}", node='PlotCode', state=LogState.FINISH)
+        except:
+            LoggerManager.log_flow_metric(node='PlotCode', state=LogState.ERROR)
         return state
     
     def __call__(self, *args: Any, **kwds: Any) -> Any:
--- steps/step4_data_storytelling.py
+++ steps/step4_data_storytelling.py
@@ -17,41 +17,43 @@ class DataStorytellingNode:
 
     def forward(self, state: AgentState):
         LoggerManager.log_flow(f"Start with {self.llm.model_name}", node=self.__class__.__name__, state=LogState.START)
-
-        image_path = state["plot_generator_results"]["plot_filename"]
-        base64_image = self.encode_image(image_path)
-        question = state['question']
-        messages = [
-            HumanMessage(
-                content=[
-                    {
-                        "type": "text",
-                        "text": (
-                            "As an expert data analyst, using the initial question and the generated plot, provide a comprehensive and professional insight that answers the user's initial question."
-                            "Ensure the narrative is suitable for a workplace environment, informative, correct, and complete."
-                            "The insight should interpret the data shown in the plot, highlighting key trends, patterns, and anomalies. "
-                            "Include relevant context, potential implications, and actionable recommendations based on the data. "
-                            "The storytelling should be engaging and thorough, offering a clear explanation and meaningful conclusions that add value to the user's understanding of the data."
-                            f"\n\ninitial question: {question}"
-                        ),
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {"url": f"data:image/png;base64,{base64_image}"},
-                    },
-                ]
-            ),
-        ]
-        # prompt_template = ChatPromptTemplate.from_messages(messages)
-        # chain = prompt_template | self.llm | StrOutputParser()
-        # results = chain.invoke({"base64_image": base64_image})
-        results = self.llm.llm.invoke(messages)
-        state["data_storytelling_results"] = results.content
-        LoggerManager.log_flow(
-            f"Storytelling Results: {results}",
-            node=self.__class__.__name__,
-            state=LogState.RESPONSE,
-        )
+        try:
+            image_path = state["plot_generator_results"]["plot_filename"]
+            base64_image = self.encode_image(image_path)
+            question = state['question']
+            messages = [
+                HumanMessage(
+                    content=[
+                        {
+                            "type": "text",
+                            "text": (
+                                "As an expert data analyst, using the initial question and the generated plot, provide a comprehensive and professional insight that answers the user's initial question."
+                                "Ensure the narrative is suitable for a workplace environment, informative, correct, and complete."
+                                "The insight should interpret the data shown in the plot, highlighting key trends, patterns, and anomalies. "
+                                "Include relevant context, potential implications, and actionable recommendations based on the data. "
+                                "The storytelling should be engaging and thorough, offering a clear explanation and meaningful conclusions that add value to the user's understanding of the data."
+                                f"\n\ninitial question: {question}"
+                            ),
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": f"data:image/png;base64,{base64_image}"},
+                        },
+                    ]
+                ),
+            ]
+            # prompt_template = ChatPromptTemplate.from_messages(messages)
+            # chain = prompt_template | self.llm | StrOutputParser()
+            # results = chain.invoke({"base64_image": base64_image})
+            results = self.llm.llm.invoke(messages)
+            state["data_storytelling_results"] = results.content
+            LoggerManager.log_flow(
+                f"Storytelling Results: {results}",
+                node=self.__class__.__name__,
+                state=LogState.RESPONSE,
+            )
+        except:
+            LoggerManager.log_flow_metric(node=self.__class__.__name__, state=LogState.ERROR)
         return state
 
     def encode_image(self, image_path: str):
